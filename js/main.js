73004d87655be4b8b0ea6e7221d9704ad1293631
Newton Bii <biinewton382@gmail.com>
2018-08-06 21:18:33 +0300

    Modified javascript and css files.

--- js/main.js
+++ js/main.js
@@ -1,35 +1,17 @@
-(function ($) {
-    "use strict";
-
-
-    jQuery(document).ready(function ($) {
-
-
-        /*---------------------------------------------*
-        * Carousel
-        ---------------------------------------------*/
-        $('#Carousel').carousel({
-                interval: 5000,
-                item: 2
-            })
-            /*------------------------*/
-
+$(document).ready(function() {
+    /* Navigation smooth scroll */
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top-30
+            },1000 );
+            return false;
+          }
+        }
+      });
     });
-
-    /*---------------------------------------------*
-        * STICKY scroll
-    ---------------------------------------------*/
-
-    $.localScroll();
-
-    /**************************/
-
-
-    jQuery(window).load(function () {
-
-
-    });
-
-
-
-}(jQuery));
+  });
